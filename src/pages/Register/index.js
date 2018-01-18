e6c7f5d9ac0a58f564ed22ec40f8480ec3680c4c
Marion Menzl <mmenzl.mmt-b2016@fh-salzburg.ac.at>
2018-01-18 23:31:36 +0100

    Removed input lines except password and email

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import { Component } from 'react';
-import classNames from 'classnames';
 import SocialBar from '../../components/SocialBar';
 import LinkBar from '../../components/LinkBar';
-import HeaderLogo from '../../components/HeaderLogo';
 import InputLine from '../../components/InputLine';
 import LargeButton from '../../components/LargeButton';
 import LogoLight from '../../images/logo-light.png';
@@ -15,10 +13,8 @@ export default class Register extends Component {
             <React.Fragment>
                 <img className="logo-login-dark" src={LogoLight} alt="Campus Brett Logo" />
                 <div className="register-container">
-                    <InputLine type="text" placeholder="Name" name="name" id="register-name" />
                     <InputLine type="email" placeholder="Email" name="email" id="register-email" />
                     <InputLine type="password" placeholder="Passwort" name="password" id="register-password" />
-                    <InputLine type="password" placeholder="Passwort wiederholen" name="passwordconf" id="register-password-conf" />
                     <LargeButton theme="light" text="REGISTRIEREN" id="register-button"/>
                 </div>
                 <SocialBar />
