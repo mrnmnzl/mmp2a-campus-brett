eaf0165a440da3f69a458ee0205dba7e5acbe023
Marion Menzl <mmenzl.mmt-b2016@fh-salzburg.ac.at>
2018-01-18 23:32:39 +0100

    Removed comments

--- src/index.js
+++ src/index.js
@@ -6,14 +6,3 @@ import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
-
-// var config = {
-//     apiKey: "AIzaSyAr-6UZeRjobk1o8RWF-M375dDjrH33E3o",
-//     authDomain: "campus-brett.firebaseapp.com",
-//     databaseURL: "https://campus-brett.firebaseio.com",
-//     projectId: "campus-brett",
-//     storageBucket: "",
-//     messagingSenderId: "719539293613"
-// };
-    
-// firebase.initializeApp(config);
