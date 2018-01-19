98ebb2e4c25e2f110e98d1737c5bd84b84350653
Marion Menzl <mmenzl.mmt-b2016@fh-salzburg.ac.at>
2018-01-19 12:13:38 +0100

    Implement Register

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -6,16 +6,52 @@ import InputLine from '../../components/InputLine';
 import LargeButton from '../../components/LargeButton';
 import LogoLight from '../../images/logo-light.png';
 import './Register.css';
+import authentification from '../../services/Authentification';
 
 export default class Register extends Component {
+    state = {
+        email: '',
+        password: ''
+    };
+
+    handleEmailChange = event => {
+        this.setState({
+            email: event.target.value
+        });
+    };
+
+    handlePasswordChange = event => {
+        this.setState({
+            password: event.target.value
+        });
+    };
+
+    handleSubmit = () => {
+        authentification.register(this.state.email, this.state.password);
+    };
+
     render() {
         return (
             <React.Fragment>
                 <img className="logo-login-dark" src={LogoLight} alt="Campus Brett Logo" />
                 <div className="register-container">
-                    <InputLine type="email" placeholder="Email" name="email" id="register-email" />
-                    <InputLine type="password" placeholder="Passwort" name="password" id="register-password" />
-                    <LargeButton theme="light" text="REGISTRIEREN" id="register-button"/>
+                    <InputLine
+                        type="email"
+                        value={this.state.email}
+                        placeholder="Email"
+                        name="email"
+                        id="register-email"
+                        onChange={this.handleEmailChange}
+                    />
+                    <InputLine
+                        type="password"
+                        value={this.state.password}
+                        placeholder="Passwort"
+                        name="password"
+                        id="register-password"
+                        onChange={this.handlePasswordChange}
+                    />
+                    <LargeButton theme="light" text="REGISTRIEREN" id="register-button" onClick={this.handleSubmit}/>
                 </div>
                 <SocialBar />
                 <LinkBar text="LOGIN" path="/login" />
