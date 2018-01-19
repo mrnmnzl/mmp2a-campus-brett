5627ebb63af040a4d1acf044fdfb7ef74ef911fc
Marion Menzl <mmenzl.mmt-b2016@fh-salzburg.ac.at>
2018-01-19 12:11:52 +0100

    Renamed Authentication file

---
+++ src/services/Authentification.js
@@ -0,0 +1,37 @@
+import * as firebase from 'firebase';
+
+const config = {
+    apiKey: 'AIzaSyAr-6UZeRjobk1o8RWF-M375dDjrH33E3o',
+    authDomain: 'campus-brett.firebaseapp.com',
+    databaseURL: 'https://campus-brett.firebaseio.com',
+    projectId: 'campus-brett',
+    storageBucket: 'campus-brett.appspot.com',
+    messagingSenderId: '719539293613'
+};
+
+class Authentication {
+    constructor() {
+        firebase.initializeApp(config);
+        firebase.auth().onAuthStateChanged(firebaseUser => {
+            if (firebaseUser) {
+                console.log(firebaseUser);
+            } else {
+                console.log('Not logged in!');
+            }
+        });
+    }
+
+    login(email, password) {
+        const auth = firebase.auth();
+        const promise = auth.signInWithEmailAndPassword(email, password);
+        promise.catch(e => console.log(e.message));
+    }
+
+    register(email, password) {
+        const auth = firebase.auth();
+        const promise = auth.createUserWithEmailAndPassword(email, password);
+        promise.catch(e => console.log(e.message));
+    }
+}
+
+export default new Authentication();
