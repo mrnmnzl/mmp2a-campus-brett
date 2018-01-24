e3a35f921380936874e47bbbff25a653883b9b04
Marion Menzl <mmenzl.mmt-b2016@fh-salzburg.ac.at>
2018-01-24 23:02:51 +0100

    Update NewPost component

--- src/pages/NewPost/index.js
+++ src/pages/NewPost/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Component } from 'react';
 import HeaderIcon from '../../components/HeaderIcon';
+import RadioCategory from '../../components/RadioCategory';
 
 
-export default class Messenger extends Component {
+export default class NewPost extends Component {
     render() {
         return (
             <React.Fragment>
                 <HeaderIcon text="NEUEN BEITRAG VERFASSEN" icon="close"/>
-                <form>
-
+                <form className="container-new-post">
+                    <RadioCategory/>
                 </form>
             </React.Fragment>
         );
