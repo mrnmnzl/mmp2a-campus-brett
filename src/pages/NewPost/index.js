5cd8b0cfcf3819299644903645752a87b7f85894
Marion Menzl <mmenzl.mmt-b2016@fh-salzburg.ac.at>
2018-01-25 13:24:34 +0100

    Add headlines and style

--- src/pages/NewPost/index.js
+++ src/pages/NewPost/index.js
@@ -2,15 +2,19 @@ import React from 'react';
 import { Component } from 'react';
 import HeaderIcon from '../../components/HeaderIcon';
 import RadioCategory from '../../components/RadioCategory';
-
+import Headline from '../../components/Headline';
+import './NewPost.css';
 
 export default class NewPost extends Component {
     render() {
         return (
             <React.Fragment>
-                <HeaderIcon text="NEUEN BEITRAG VERFASSEN" icon="close"/>
+                <HeaderIcon text="NEUEN BEITRAG VERFASSEN" icon="close" />
                 <form className="container-new-post">
-                    <RadioCategory/>
+                    <Headline text="KATEGORIE" />
+                    <RadioCategory />
+                    <Headline text="TITEL" />
+                    <Headline text="BESCHREIBUNG" />
                 </form>
             </React.Fragment>
         );
