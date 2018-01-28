f2e7f037af0f0c89c570bbe70fcf1116ec783d95
Marion Menzl <mmenzl.mmt-b2016@fh-salzburg.ac.at>
2018-01-28 14:18:02 +0100

    Update Styles

--- src/components/InputArea/index.js
+++ src/components/InputArea/index.js
@@ -5,7 +5,7 @@ import './InputArea.css';
 export default class InputArea extends Component {
     render() {
         return (
-            <textarea className="input-area m" type="textarea" placeholder={this.props.placeholder} rows="4" cols="50"/>
+            <textarea className="input-area" type="textarea" placeholder={this.props.placeholder} rows="4" cols="50"/>
         );
     }
 }
