e143164a512fe293f8dcb273ab03f78a63cf7b96
Marion Menzl <mmenzl.mmt-b2016@fh-salzburg.ac.at>
2018-01-29 13:17:21 +0100

    Add theme to post

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import { Component } from 'react';
+import classNames from 'classnames';
 import './Post.css';
 
 export default class Post extends Component {
     render() {
+        const classList = classNames(
+            'post-container',
+            this.props.theme === "search" ? "bg-search" : "bg-find"
+        );
         return (
-            <div className="post-container bg-search">
+            <div className={classList}>
                 <p className="post-name">{this.props.name}</p>
                 <p className="post-time">{this.props.time}</p>
                 <p className="post-title">{this.props.title}</p>
                 <p className="post-description">{this.props.description}</p>
                 <div className="post-tags-container" id="tag-container">
+                    <span className="tag">{this.props.tag}</span>
                 </div>
             </div>
         );
     }
 }
+//name, time, title, description, tag
