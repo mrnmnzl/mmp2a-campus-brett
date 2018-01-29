65f0c74c39cf2d2000f4c34524d282593d847e85
Marion Menzl <mmenzl.mmt-b2016@fh-salzburg.ac.at>
2018-01-29 15:32:58 +0100

    Add className functionality

--- src/components/CategoryBar/index.js
+++ src/components/CategoryBar/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import { Component } from 'react';
 import './CategoryBar.css';
+import classNames from 'classnames';
+import { Link } from 'react-router-dom';
 
 export default class CategoryBar extends Component {
     render() {
+        const classListSearch = classNames(
+            this.props.underline === "search" ? "underline" : "",
+            'button-category'
+        );
+        const classListOffer = classNames(
+            this.props.underline === "offer" ? "underline" : "",
+            'button-category'
+        );
         return (
             <div className="bar-category">
-                <button className="button-category">Gesucht</button>
-                <button className="button-category">Geboten</button>
+                <Link to="/"><button className={classListSearch}>Gesucht</button></Link>
+                <Link to="/offer"><button className={classListOffer}>Geboten</button></Link>
                 <span className="icon-search"></span>
             </div>
         );
