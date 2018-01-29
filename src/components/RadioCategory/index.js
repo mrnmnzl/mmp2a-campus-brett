2c52fca17c97ea9b8cea9392c5da5d0ef44d8140
Marion Menzl <mmenzl.mmt-b2016@fh-salzburg.ac.at>
2018-01-29 13:17:52 +0100

    Change find to offer

--- src/components/RadioCategory/index.js
+++ src/components/RadioCategory/index.js
@@ -12,11 +12,11 @@ export default class RadioCategory extends Component {
                         <span />
                     </span>Suchen
                 </label>
-                <input className="input-radio input-radio-find" type="radio" id="fi" name="category" value="find" onChange={this.props.onChange}/>
+                <input className="input-radio input-radio-find" type="radio" id="fi" name="category" value="offer" onChange={this.props.onChange}/>
                 <label className="input-radio-label" htmlFor="fi">
                     <span>
                         <span />
-                    </span>Finden
+                    </span>Bieten
                 </label>
             </React.Fragment>
         );
