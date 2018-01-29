86c9a65c7c9983bb40e46033e9dd01c8e8a8e08a
Anna Kutschka <akutschka.mmt-b2016@fh-salzburg.ac.at>
2018-01-29 20:42:18 +0100

    Chat functions for Messenger - not working

--- src/pages/Chatroom/index.js
+++ src/pages/Chatroom/index.js
@@ -1,3 +1,4 @@
+import * as firebase from 'firebase';
 import React from 'react';
 import { Component } from 'react';
 import HeaderIcon from '../../components/HeaderIcon';
@@ -5,33 +6,79 @@ import InputField from '../../components/InputField';
 import './Chatroom.css'
 import authentification from '../../services/Authentification';
 
-
-
 export default class Chatroom extends Component {
     state = {
-        message: ''
-    };
+        message: '',
+        messages: []
+    }
 
-    handleMessage = event => {
+    //live - cicle - method -> System calls this function is the conection to firebase
+    //reload - must shown in Console
+    componentDidMount(){
+        console.log('componentDidMount');
+        firebase.database().ref('messages/').on('value', (snapshot)=> {
+
+            const currentMessages = snapshot.val()
+
+            if(currentMessages != null){
+                this.setState({
+                    messages: currentMessages
+                })
+            }
+        })
+    }
+
+    handleMessage(event) {
+        console.log('Message update: ' + event.target.value)
         this.setState({
             message: event.target.value
-        });
-    };
+        })
+    }
+
+    handleSubmit(event){
+        console.log('submit Message: ' + this.state.message)
+        //preparing next message
+        const nextMessage = {
+            id: this.state.message.length,
+            text: this.state.message
+        }
+
+        //connect to firebase
+        firebase.database().ref('messages/' + nextMessage.id).set(nextMessage);
+
+
+        // //list of messages
+        // var list = Object.assign([], this.state.messages);
+        // //Message in den Chat schicken
+        // list.push(nextMessage);
+        // this.setState({
+        //     messages: list
+        // })
+    }
 
     handleBack = event => {
-        this.props.history.push('/messenger');
+        this.props.history.push('/messenger')
     }
 
     render() {
+        const currentMessage = this.state.messages.map((message, i) => {
+            return (
+                <li key={message.id}>{message.text}</li>
+            )
+        })
+
         return (
             <React.Fragment>
                 <HeaderIcon text="CHATROOM" icon="back" onClick={this.handleBack}/>
                 <div className="container-chat">
+                    <ol className="chat-messages">
+                        {currentMessage}
+                    </ol>
                     <div className="chat-bar">
                         <div className="chat-input" >
                             <InputField placeholder="Nachricht verfassen..." onChange={this.handleMessage} value={this.state.message}/>
                         </div>
-                        <span className="send-icon icon-arrow-up" ></span>
+                        <span className="send-icon icon-arrow-up" onClick={this.handleSubmit} ></span>
                     </div>
                 </div>
             </React.Fragment>
