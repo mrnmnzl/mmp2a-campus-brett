6359a10dd67ef535be03c5ed446bef96fb8d9d5d
Anna Kutschka <akutschka.mmt-b2016@fh-salzburg.ac.at>
2018-01-29 16:49:24 +0100

    from chat to messages root

--- src/pages/Chatroom/index.js
+++ src/pages/Chatroom/index.js
@@ -18,16 +18,20 @@ export default class Chatroom extends Component {
         });
     };
 
+    handleBack = event => {
+        this.props.history.push('/messenger');
+    }
+
     render() {
         return (
             <React.Fragment>
-                <HeaderIcon text="CHATROOM" icon="back" />
+                <HeaderIcon text="CHATROOM" icon="back" onClick={this.handleBack}/>
                 <div className="container-chat">
                     <div className="chat-bar">
                         <div className="chat-input" >
                             <InputField placeholder="Nachricht verfassen..." onChange={this.handleMessage} value={this.state.message}/>
                         </div>
-                        <span className="send-icon icon-arrow-up"></span>
+                        <span className="send-icon icon-arrow-up" ></span>
                     </div>
                 </div>
             </React.Fragment>
