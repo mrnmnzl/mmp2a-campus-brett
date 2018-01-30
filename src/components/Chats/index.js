28b3188fcf7da56d5c6d446211e31b7699bf40b0
Anna Kutschka <akutschka.mmt-b2016@fh-salzburg.ac.at>
2018-01-30 19:19:28 +0100

    added space for profilpicture

--- src/components/Chats/index.js
+++ src/components/Chats/index.js
@@ -7,7 +7,9 @@ export default class Chats extends Component {
     render() {
         return (
             <div className="chat-text">
-                {/* <svg width="70" height="70">
+                
+                {/* Profilbild fehlt noch!
+                <svg width="70" height="70">
                     <rect x="5" y="5" width="70" height="70" style="fill:blue;fill-opacity:0.1" />
                 </svg> */}
                 <p>{this.props.text}</p>
