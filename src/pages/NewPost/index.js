db82150d2f8bc75d541eb55bfe493b3a4c399fed
Marion Menzl <mmenzl.mmt-b2016@fh-salzburg.ac.at>
2018-01-30 18:34:42 +0100

    No fix for name on post

--- src/pages/NewPost/index.js
+++ src/pages/NewPost/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Component } from 'react';
-import * as firebase from 'firebase';
+// import * as firebase from 'firebase';
 import HeaderIcon from '../../components/HeaderIcon';
 import RadioCategory from '../../components/RadioCategory';
 import Headline from '../../components/Headline';
@@ -11,91 +11,101 @@ import LargeButton from '../../components/LargeButton';
 import './NewPost.css';
 import dataHandling from '../../services/DataHandling';
 import { history } from '../../App';
-import { userId } from '../../components/EnsureLoggedInContainer';
 
 export default class NewPost extends Component {
     state = {
         category: 'search',
-        name: '',
         title: '',
         description: '',
         tag: ''
-    }
-    
+    };
+
+    componentDidMount = () => {};
+
     handleCategory = event => {
         this.setState({
             category: event.target.value
         });
         //setTimeout(() => console.log(this.state.category), 0);
-    }
+    };
 
     handleTitle = event => {
         this.setState({
             title: event.target.value
         });
         //setTimeout(() => console.log(this.state.title), 0);
-    }
+    };
 
     handleDescription = event => {
         this.setState({
             description: event.target.value
         });
         //setTimeout(() => console.log(this.state.description), 0);
-    }
+    };
 
     handleTag = value => {
         this.setState({
             tag: value
         });
-    }
+    };
 
     handleClose = () => {
         history.goBack();
-    }
-
-    getUserName = () => {
-        const id = userId;
-        firebase
-            .database()
-            .ref('/users/' + id)
-            .once('value')
-            .then(snapshot => {
-                this.setState({
-                    name: snapshot.val()
-                });
-            });   
-    }
+    };
 
     handleSubmit = () => {
-        dataHandling.createNewPost(this.state.name.username, this.state.category, this.state.title, this.state.description, this.state.tag)
-    }
+        // let userId = firebase.auth().currentUser.uid;
+        let username = "Max Mustermann";
+        // firebase
+        // .database()
+        // .ref('/users/' + userId)
+        // .once('value')
+        // .then(snapshot => {
+        //     username = snapshot.val().username;
+        // });
+
+        dataHandling.createNewPost(username, this.state.category, this.state.title, this.state.description, this.state.tag);
+    };
 
     render() {
         return (
             <React.Fragment>
-                <HeaderIcon text="NEUEN BEITRAG VERFASSEN" icon="close" onClick={this.handleClose}/>
+                <HeaderIcon text="NEUEN BEITRAG VERFASSEN" icon="close" onClick={this.handleClose} />
                 <form className="container-new-post">
                     <Headline text="KATEGORIE" />
                     <div className="radio-container" onChange={this.handleCategory}>
                         <RadioCategory />
                     </div>
                     <Headline text="TITEL" />
-                    <InputField placeholder="Titel..." onChange={this.handleTitle} value={this.state.title}/>
+                    <InputField placeholder="Titel..." onChange={this.handleTitle} value={this.state.title} />
                     <Headline text="BESCHREIBUNG" />
-                    <InputArea placeholder="Beschreibung einfügen" onChange={this.handleDescription} value={this.state.description}/>
+                    <InputArea
+                        placeholder="Beschreibung einfügen"
+                        onChange={this.handleDescription}
+                        value={this.state.description}
+                    />
                     <Headline text="TAG" />
                     <div className="tags-container">
-                        <CategoryTag text="werkzeug" onClick={this.handleTag} category={this.state.category}/>
-                        <CategoryTag text="hilfe" onClick={this.handleTag} category={this.state.category}/>
-                        <CategoryTag text="mitfahrgelegenheit" onClick={this.handleTag} category={this.state.category}/>
-                        <CategoryTag text="lebensmittel" onClick={this.handleTag} category={this.state.category}/>
-                        <CategoryTag text="kleidung" onClick={this.handleTag} category={this.state.category}/>
-                        <CategoryTag text="unterhaltung" onClick={this.handleTag} category={this.state.category}/>
-                        <CategoryTag text="putzen" onClick={this.handleTag} category={this.state.category}/>
-                        <CategoryTag text="party" onClick={this.handleTag} category={this.state.category}/>
+                        <CategoryTag text="werkzeug" onClick={this.handleTag} category={this.state.category} />
+                        <CategoryTag text="hilfe" onClick={this.handleTag} category={this.state.category} />
+                        <CategoryTag
+                            text="mitfahrgelegenheit"
+                            onClick={this.handleTag}
+                            category={this.state.category}
+                        />
+                        <CategoryTag text="lebensmittel" onClick={this.handleTag} category={this.state.category} />
+                        <CategoryTag text="kleidung" onClick={this.handleTag} category={this.state.category} />
+                        <CategoryTag text="unterhaltung" onClick={this.handleTag} category={this.state.category} />
+                        <CategoryTag text="putzen" onClick={this.handleTag} category={this.state.category} />
+                        <CategoryTag text="party" onClick={this.handleTag} category={this.state.category} />
                     </div>
                     <div className="button-container">
-                        <LargeButton className="save-new-post-button" text="BEITRAG SPEICHERN" theme="light" onClick={this.handleSubmit}/>
+                        <LargeButton
+                            className="save-new-post-button"
+                            text="BEITRAG SPEICHERN"
+                            theme="light"
+                            onClick={this.handleSubmit}
+                        />
                     </div>
                 </form>
             </React.Fragment>
