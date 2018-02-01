3c652f075411eb178b0f54c500ec387c8ebe950f
Marion Menzl <mmenzl.mmt-b2016@fh-salzburg.ac.at>
2018-02-01 16:47:54 +0100

    Update SavedPosts

--- src/pages/SavedPosts/index.js
+++ src/pages/SavedPosts/index.js
@@ -1,97 +1,96 @@
 import React from 'react';
 import { Component } from 'react';
+import * as firebase from 'firebase';
 import Navigation from '../../components/Navigation';
 import HeaderText from '../../components/HeaderText';
 import Post from '../../components/Post';
-import { userId } from '../../components/EnsureLoggedInContainer/index';
 import { Link } from 'react-router-dom';
 import dataHandling from '../../services/DataHandling';
+import './SavedPosts.css';
 
 export default class SavedPosts extends Component {
     state = {
         posts: [],
-        savedPosts: []
+        savedPosts: [],
+        postsForDisplay: []
     }
 
     componentWillMount() {
-        dataHandling.addDataChangeListener('posts', this.setPostsState);
+        const userId = firebase.auth().currentUser.uid;
         const path = 'savedPosts/' + userId;
-        dataHandling.addDataChangeListener(path, this.setSavedPostsState);
+        dataHandling.addDataChangeListener('posts', this.handlePostsDataChange);
+        dataHandling.addDataChangeListener(path, this.handleSavedPostsDataChange);
     }
 
-    setPostsState = (posts) => {
-        this.setState({
-            posts: posts.val()
-        })
+    handlePostsDataChange = (data) => {
+        if(data.hasChildren()) {
+            this.setState({
+                posts: data.val()
+            });
+        }
     }
 
-    setSavedPostsState = (posts) => {
-        this.setState({
-            savedPosts: posts.val()
-        });
+    handleSavedPostsDataChange = (data) => {
+        if(data.hasChildren()) {
+            this.setState({
+                savedPosts: data.val()
+            });
+        }
     }
-
-    // handleSavedPosts = (savedPosts, posts) => {
-    //     const database = firebase.database();
-    //     const savedPosts = database.ref('/savedPosts/' + userId + "/");
-    //     const posts = database.ref('/posts/');
-
-    //     if (savedPosts === null) {
-    //         return;
-    //     }
-
-    //     const savedPostKeys = Object.keys(savedPosts);
-    //     const postsKeys = Object.keys(posts);
-    //     let displayedPosts = [];
-
-    //     console.log(savedPostKeys[1]);
-    //     console.log(postsKeys[1].title);
-
-    //     for (let i = 0; i < savedPostKeys.length; i++) {
-    //         const k = savedPostKeys[i];
-    //         for(let postKey in postsKeys) {
-    //             if(postKey === savedPostKeys[i].postId) {
-    //                 const name = postKey.name;
-    //                 const title = postKey.title;
-    //                 const description = postKey.description;
-    //                 const tag = postKey.tag;
-    //                 const user = postKey.user;
-    //                 let time = postKey.time;
-    //                 if (time != null) {
-    //                     time = time.substring(0, time.length - 3);
-    //                 }
-    //                 const category = postKey.category;
-
-    //                 displayedPosts.push({
-    //                     id: k,
-    //                     name: name,
-    //                     time: time,
-    //                     title: title,
-    //                     description: description,
-    //                     tag: tag,
-    //                     user: user,
-    //                     category: category
-    //                 });
-    //             }
-    //         }
-    //     }
-
-    //     displayedPosts.reverse();
-    //     this.setState({
-    //         displayedPosts: posts
-    //     });
-    // };
 
     render() {
-        console.log(this.state.posts);
-        console.log(this.state.savedPosts);
+        const postsForDisplay = [];
+
+        if (this.state.savedPosts !== [] && this.state.posts !== []) {
+            const savedPostsKeys = Object.keys(this.state.savedPosts);
+            const postsKeys = Object.keys(this.state.posts);
+
+            if(this.state.savedPosts.length !== 0 && this.state.posts.length !== 0) {
+                let savedPosts;
+                savedPosts = this.state.savedPosts;
+                let posts;
+                posts = this.state.posts;
+
+                for (let i = 0; i < savedPostsKeys.length; i++) {
+                    const savedPostKey = savedPostsKeys[i];
+                    const savedPostId = savedPosts[savedPostKey].postId;
+
+                    for(let j = 0; j < postsKeys.length; j++) {
+                        if(savedPostId === postsKeys[j]) {
+                            const key = postsKeys[j];
+                            const name = posts[key].name;
+                            let time = posts[key].time;
+                            time = time.substring(0, time.length - 3);
+                            const title = posts[key].title;
+                            const description = posts[key].description;
+                            const tag = posts[key].tag;
+                            const category = posts[key].category;
+
+                            postsForDisplay.push({
+                                id: key,
+                                name: name,
+                                time: time,
+                                title: title,
+                                description: description,
+                                tag: tag,
+                                category: category
+                            });
+                        }
+                    }
+                }
+            }
+        }
+
+        postsForDisplay.reverse();
 
         return (
             <React.Fragment>
                 <HeaderText text="GESPEICHERTE BEITRÄGE" />
                 <div className="posts-container-saved-posts" id="post-container">
-                    <div id="warnings"></div>
-                    {/* {this.state.posts.map(post => {
+                    <div id="warning-saved-posts">
+                        <p>Du hast noch keine Beiträge gespeichert!</p>
+                    </div>
+                    {postsForDisplay.map(post => {
                         const path = '/post/' + post.id;
                         return (
                             <Link to={path} key={post.time}>
@@ -106,7 +105,7 @@ export default class SavedPosts extends Component {
                                 />
                             </Link>
                         );
-                    })} */}
+                    })}
                 </div>
                 <Navigation />
             </React.Fragment>
