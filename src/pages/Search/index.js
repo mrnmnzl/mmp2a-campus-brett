f68fe7211e8196a585f8abb0cfd3526d3178b851
Marion Menzl <mmenzl.mmt-b2016@fh-salzburg.ac.at>
2018-02-02 00:40:51 +0100

    Add new tags and remove navigation

--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Component } from 'react';
-import Navigation from '../../components/Navigation';
 import HeaderIcon from '../../components/HeaderIcon';
 import Headline from '../../components/Headline';
 import RadioCategory from '../../components/RadioCategory';
@@ -14,7 +13,7 @@ export default class Search extends Component {
     state = {
         tag: '',
         category: ''
-    } 
+    };
 
     handleBack = () => {
         history.push('/');
@@ -24,67 +23,70 @@ export default class Search extends Component {
         this.setState({
             category: event.target.value
         });
-    }
+
+        document.getElementById('tags-container-search').style.display = 'flex';
+    };
 
     handleTag = value => {
         this.setState({
             tag: value
         });
-    }
+    };
 
     handleSubmit = value => {
         //if form is not correct
         const tag = this.state.tag;
         const category = this.state.category;
-        console.log('tag: ' + tag + ' category: ' + category);
-        if (tag === '' || category === ''){
+
+        if (tag === '' || category === '') {
             const container = document.getElementById('warnings');
             while (container.firstChild) {
                 container.removeChild(container.firstChild);
             }
             container.insertAdjacentHTML('beforeend', '<p> Bitte fülle das Formular ganz aus </p>');
-        }
-        else{
+        } else {
             const path = '/search/' + category + '/' + tag;
             history.push(path);
             console.log(path);
         }
-    }
+    };
 
     render() {
         return (
             <React.Fragment>
                 <Link to="/">
-                <HeaderIcon text="SUCHE" icon="back" onclick={this.handleBack}/>
+                    <HeaderIcon text="SUCHE" icon="back" onclick={this.handleBack} />
                 </Link>
-                <div className="container-new-post">
+                <div className="container-search">
                     <Headline text="KATEGORIE" />
                     <div className="radio-container" onChange={this.handleCategory}>
                         <RadioCategory />
                     </div>
                     <Headline text="TAG" />
-                    <div className="tags-container-search">
-                        <CategoryTag text="werkzeug" onClick={this.handleTag} category={this.state.category}/>
-                        <CategoryTag text="hilfe" onClick={this.handleTag} category={this.state.category}/>
-                        <CategoryTag text="mitfahrgelegenheit" onClick={this.handleTag} category={this.state.category}/>
-                        <CategoryTag text="lebensmittel" onClick={this.handleTag} category={this.state.category}/>
-                        <CategoryTag text="kleidung" onClick={this.handleTag} category={this.state.category}/>
-                        <CategoryTag text="unterhaltung" onClick={this.handleTag} category={this.state.category}/>
-                        <CategoryTag text="putzen" onClick={this.handleTag} category={this.state.category}/>
-                        <CategoryTag text="party" onClick={this.handleTag} category={this.state.category}/>
+                    <div className="tags-container-search" id="tags-container-search">
+                        <CategoryTag id="tag-werkzeug" text="werkzeug" onClick={this.handleTag} />
+                        <CategoryTag id="tag-hilfe" text="hilfe" onClick={this.handleTag} />
+                        <CategoryTag id="tag-mitfahrgelegenheit" text="mitfahrgelegenheit" onClick={this.handleTag} />
+                        <CategoryTag id="tag-lebensmittel" text="lebensmittel" onClick={this.handleTag} />
+                        <CategoryTag id="tag-kleidung" text="kleidung" onClick={this.handleTag} />
+                        <CategoryTag id="tag-kleidung" text="kleidung" onClick={this.handleTag} />
+                        <CategoryTag id="tag-unternehmung" text="unternehmung" onClick={this.handleTag} />
+                        <CategoryTag id="tag-unterhaltung" text="unterhaltung" onClick={this.handleTag} />
+                        <CategoryTag id="tag-haushaltsgeräte" text="haushaltsgeräte" onClick={this.handleTag} />
+                        <CategoryTag id="tag-party" text="party" onClick={this.handleTag} />
+                        <CategoryTag id="tag-pc-problem" text="pc-problem" onClick={this.handleTag} />
                     </div>
-                    <div id="warnings"></div>
+                    <div id="warnings" />
                     <div className="button-container-search">
-                        <LargeButton 
+                        <LargeButton
                             className="search-button"
-                            text="SUCHEN" 
-                            theme="light" 
+                            text="SUCHEN"
+                            theme="light"
                             onClick={this.handleSubmit}
                             id="search-button"
                         />
                     </div>
                 </div>
-                <Navigation />
             </React.Fragment>
         );
     }
